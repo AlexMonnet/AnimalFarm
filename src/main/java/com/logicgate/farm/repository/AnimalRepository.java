21f73a606b8e2544ec974ea8261b467cedf2846d
Alex Monnet <alex.monnet@e-signum.com>
2019-11-08 19:46:15 -0600

    First implementation passing tests.

--- src/main/java/com/logicgate/farm/repository/AnimalRepository.java
+++ src/main/java/com/logicgate/farm/repository/AnimalRepository.java
@@ -1,13 +1,33 @@
 package com.logicgate.farm.repository;
 
 import com.logicgate.farm.domain.Animal;
+import com.logicgate.farm.domain.Barn;
+import com.logicgate.farm.domain.Color;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
-  // additional methods can be defined here
+  /**
+   * This method finds all animals that have a favorite color equal to
+   *  the parameter favoriteColor. This method is filled out using JPA
+   *  Query creation
+   * @param favoriteColor Animals' favorite color
+   * @return List of animals with a favorite color of favoriteColor
+   */
+  public List<Animal> findByFavoriteColor(Color favoriteColor);
+
+  /**
+   * This method finds all animals that inhabit the barn passed in
+   *  as a parameter. This method is filled out using JPA Query creation
+   * @param barn Animals' barn
+   * @return List of animals that inhabit the barn
+   */
+  public List<Animal> findByBarn(Barn barn);
+
 
 }
--- src/main/java/com/logicgate/farm/service/AnimalServiceImpl.java
+++ src/main/java/com/logicgate/farm/service/AnimalServiceImpl.java
@@ -1,14 +1,22 @@
 package com.logicgate.farm.service;
 
 import com.logicgate.farm.domain.Animal;
+import com.logicgate.farm.domain.Barn;
+import com.logicgate.farm.domain.Color;
 import com.logicgate.farm.repository.AnimalRepository;
 import com.logicgate.farm.repository.BarnRepository;
+import com.zaxxer.hikari.util.ConcurrentBag;
 
+import org.h2.mvstore.ConcurrentArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.management.RuntimeErrorException;
 
 @Service
 @Transactional
@@ -37,8 +45,15 @@ public class AnimalServiceImpl implements AnimalService {
 
   @Override
   public Animal addToFarm(Animal animal) {
-    // TODO: implementation of this method
-    return null;
+    // List<Barn> barns = barnRepository.findByColor(animal.getFavoriteColor());
+    // Color barnColor = animal.getFavoriteColor();
+    // if(barns.size() == 0){
+    //  barns.add(barnRepository.saveAndFlush(new Barn("Barn " + barnColor.toString(), barnColor)));
+    // }
+    // animal.setBarn(barns.get(0));
+    animalRepository.saveAndFlush(animal);
+    redistributeAnimalsOfBarnColor(animal.getFavoriteColor());
+    return animal;
   }
 
   @Override
@@ -48,11 +63,90 @@ public class AnimalServiceImpl implements AnimalService {
 
   @Override
   public void removeFromFarm(Animal animal) {
-    // TODO: implementation of this method
+    animalRepository.delete(animal);
+    redistributeAnimalsOfBarnColor(animal.getFavoriteColor());
   }
 
   @Override
   public void removeFromFarm(List<Animal> animals) {
     animals.forEach(animal -> removeFromFarm(animalRepository.getOne(animal.getId())));
+  }
+
+  /**
+   * This method redistributes all animals into barns
+   * @param barnColor
+   */
+  private void redistributeAnimalsOfBarnColor(Color barnColor) {
+    List<Barn>   barns   = barnRepository.findByColor(barnColor);
+    final List<Animal> animals = animalRepository.findByFavoriteColor(barnColor);
+
+    //If there are no animals, clear out the barns
+    if(animals.size() == 0){
+      barnRepository.deleteAll(barns);
+    } else {
+      //If we don't have any barns, go ahead and create one so that we can pull the barn capacity property.
+      if(barns.size() == 0){
+        barns.add(barnRepository.saveAndFlush(new Barn("Barn " + barnColor.toString() + " 0", barnColor)));
+      }
+
+      final int barnCapacity = barns.get(0).getCapacity();
+      final int necessaryNumberOfBarns = findNecessaryNumberOfBarns(barnCapacity, animals.size());
+      List<Barn> barnsToRemove = new ArrayList<>();
+      int flag = 0;
+      //Adjust the number of barns to fit the minimum necessary
+      if(barns.size() < necessaryNumberOfBarns){
+        Integer barnNumbering = barns.size() - 1;
+        while (barns.size() < necessaryNumberOfBarns){
+          barns.add(barnRepository.saveAndFlush(new Barn("Barn " + barnColor.toString() + " " + barnNumbering.toString(), barnColor)));
+          barnNumbering++;
+        }
+      } else if (barns.size() > necessaryNumberOfBarns){
+        flag = 1;
+        barns.sort((Barn b1, Barn b2) -> b1.getName().compareTo(b2.getName()));
+        barnsToRemove = barns.subList(necessaryNumberOfBarns, barns.size());
+        barns = barns.subList(0, necessaryNumberOfBarns);
+      }
+
+      //reset which barns which animals are located in
+      animals.stream().forEach(animal -> {
+        animal.setBarn(null);
+      });
+
+      //Once the proper number of barns are established, we redistribute the animals.
+      int iterator = 0;
+      for (Animal animal : animals){
+        int barnToAddTo = iterator % barns.size();
+        Barn animalsBarn = barns.get(barnToAddTo);
+
+        if(animalRepository.findByBarn(animalsBarn).size() > animalsBarn.getCapacity()){
+          throw new RuntimeException("Trying to add more animals to the barn than it can hold! iterator: " + iterator +
+          " barnToAddTo: " + barnToAddTo + " barns.size(): " + barns.size() + " necessaryNumberOfBarns: " + necessaryNumberOfBarns +
+          " animals.size(): " + animals.size() + " flag: " + flag);
+        }
+
+        animal.setBarn(animalsBarn);
+        iterator++;
+      }
+
+      //Once the animals are put into new barns, remove any unused ones.
+      barnsToRemove.stream().forEach(barnRepository::delete);
+
+      for (Barn barn : barns){
+        if(animalRepository.findByBarn(barn).size() == 0){
+          throw new RuntimeException("Empty barn! barn: " + barn.toString() + " flag: " + flag);
+        }
+      }
+    }
+  }
+
+  private int findNecessaryNumberOfBarns(final int barnCapacity, final int numberOfAnimals) {
+    int numberOfBarns = numberOfAnimals / barnCapacity;
+
+    //If there was any remainder from the integer division, bump the number of barns by 1
+    if (numberOfAnimals % barnCapacity != 0)
+    {
+      numberOfBarns++;
+    }
+    return numberOfBarns;
   }
 }
